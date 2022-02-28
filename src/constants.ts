96353e9d2f357455b22456af974def59d8d488df
Pierre van Male <vmalep@protonmail.com>
2022-02-28 09:18:07 +0100

    switch to local BE

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const API_URL = "https://be-sbf.herokuapp.com/api";
-//export const API_URL = "http://localhost:1337/api";
+//export const API_URL = "https://be-sbf.herokuapp.com/api";
+export const API_URL = "http://localhost:1337/api";
 export const TOKEN_KEY = "refine-auth";
